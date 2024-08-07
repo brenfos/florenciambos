51e2128a3127dfcb0255ce1027d6aff817966218
Misaki <foschibrenda@gmail.com>
2024-08-07 16:49:50 -0300

    correcciones

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
             iniciospeach: "Traduce tus visiones al mundo. Da vida a tus creaciones audiovisuales.",
             inicioenes: "TRADUCCIONES INGLÉS - ESPAÑOL",
             /*Sobre mí*/
-            abouttext: "Soy traductora Pública y Audiovisual. Recibida en la Universidad CAECE de Mar del Plata en el año 2012, y desde entonces que me dedico activamente a la traducción. ",
+            abouttext: "Soy traductora Pública y Audiovisual. Recibida en la Universidad CAECE de Mar del Plata en el año 2013, y desde entonces me dedico activamente a la traducción. ",
             abouttitle: "Traducciones Inglés - Español",
             abouttext2: "¿Por qué elegí la traducción?",
             abouttext3: "Descubrí mi pasión por la lengua inglesa y la lingüística a muy temprana edad. Desde pequeña siempre consumí todo tipo de contenido en inglés, me interesaba escuchar cómo hablaban y entender culturas distintas a la mía. Lo primero que me cautivó de la lengua inglesa fue la música – me fascinaba entender lo que cantaban mis bandas favoritas, para mí era un superpoder. De adolescente, me empezó a maravillar el cine. <br> La magia de los subtítulos me permitió sumergirme aún más en mundos que, de otro modo, habrían permanecido separados. Esta fascinación por el cine y la música, junto con mi amor por el inglés, me llevaron a descubrir una carrera que une todas mis pasiones: la <strong>traducción audiovisual</strong>. <br>A lo largo de los años, he tenido el privilegio de trabajar en proyectos diversos que abarcan desde películas y series hasta videos empresariales y documentales, siempre con el objetivo de transmitir fielmente la esencia y el estilo del contenido original. Mi conocimiento de las diferentes culturas me permite localizar adecuadamente el contenido para la audiencia meta.<br> Mi formación académica y mi experiencia profesional me han permitido perfeccionar mis habilidades en este campo, y cada nuevo proyecto es una oportunidad para fusionar mi amor por la lengua y la cultura con mi deseo de conectar a personas de diferentes orígenes a través del poder de la traducción audiovisual.",
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
             caeceTitle2: " Mar del Plata, Argentina",
             swornTranslator: "Traductora Pública",
             techTranslator: "Traductora Técnico-Científica",
-            caeceDates1: "Inglés - Español, 2008 - 2013",
+            caeceDates1: "Inglés - Español, 2008 - 2012",
             bsasTitle: "As. de Traductores Públicos\n en Buenos Aires",
             swornTranslatorCourse: "Miembro del Colegio de Traductores Públicos de la Ciudad de Buenos Aires (CTPCBA)",
             /*Cursos*/
@@ -43,11 +43,11 @@ document.addEventListener('DOMContentLoaded', function () {
             curso6: "III Jornadas Internacionales de Traducción e Interpretación",   
             by6: "Disertantes: Xosé Castro Roig, Liliana Bernardita Mariotto, Damián Santilli, Mariana Costa, entre otros.",
             /*Subtitulados*/
-            titlesub: "Subtitulados",
+            titlesub: "Subtitulado",
             contentsub: "¡Transforma tu contenido con precisión y profesionalismo!", 
-            contentsub2: "¡Bienvenido a mis servicios de traducción y subtitulación! Aquí, transformaré tu contenido audiovisual en una experiencia accesible para audiencias globales:", 
-            contentsub3: "Puedo llevar tu proyecto audiovisual al siguiente nivel. Con experiencia en múltiples producciones, garantizo resultados de alta calidad con un enfoque personalizado y eficiente.",
-            titlesub2: "Ofrezco los siguientes servicios de traducción audiovisual:",
+            contentsub2: "¡Bienvenido a mis servicios de traducción y subtitulado! Aquí, transformaré tu contenido audiovisual en una experiencia accesible para audiencias globales:", 
+            contentsub3: "Puedo llevar tu proyecto audiovisual al siguiente nivel. Con experiencia en múltiples producciones, <br>garantizo resultados de alta calidad con un enfoque personalizado y eficiente.",
+            titlesub2: "Ofrezco los siguientes servicios <br>de traducción audiovisual:" ,
             subtipelis: "Subtitulado de Películas",
             subtitv: "Subtitulado de Programas<br> de TV y Animación",
             subtidocu: "Subtitulado de Producciones Documentales",
@@ -80,9 +80,10 @@ document.addEventListener('DOMContentLoaded', function () {
             docuacad: "Documentos académicos",
             financial: "Documentos financieros",
             podnota: "Poderes notariales",
-            /*Contacto*/
+
+               /*Contacto*/
             contact2: "¡Contactame desde cualquier lugar del mundo!",
-            pcontact2: "Contáctate conmigo:",
+            pcontact2: "Escribime o seguime:",
         },
         en: { 
             /*Navbar*/
@@ -104,12 +105,12 @@ document.addEventListener('DOMContentLoaded', function () {
             abouttext2: "Why did I choose translation?",
             abouttext3: "I discovered my passion for the English language and linguistics very early on. Ever since I was a little kid, I have been consuming all types of English content, I thrived in listening to the language and understanding foreign cultures. The first thing that caught my attention was music – I was delighted by the idea of understanding what my favorite bands were singing. It felt like a super power. As a teenager, I was increasingly drawn to the world of movies.<br>The magic of subtitles allowed me to immerse myself in universes that would have been kept separate otherwise. My fascination for music and movies, plus my love for the English language, made me discover a career that connects all my passions – <strong> Audiovisual Translation.</strong> <br>Throughout the years, I have had the privilege of working in different projects that range from movies and TV shows to corporate videos and documentaries. My goal has always been to faithfully transmit the essence and style of the original content, and my knowledge of the different cultures makes it easy for me to localize the content for the target audience. <br>My academic background and professional experience have been crucial in shaping my skill set. Each new project presents an opportunity to blend my passion for language, linguistics, and Anglo-Saxon culture with my commitment to bridging cultural gaps through the power of audiovisual translation.",
             abouttext4: "What are my specialties?",
-            abouttext5: "I specialize in <strong>Subtitling</strong>, <strong>Closed Captioning</strong>, <strong>SDH</strong>, and <strong>QC</strong> across a wide range of movies, TV shows, documentaries, and corporate videos. <br> Also, I am a <strong>Sworn Translator</strong>, I am enrolled in Sworn Translators Association in Buenos Aires (CTPCBA), so I am equipped to assist you with all your certified translation requirements.",
+            abouttext5: "I specialize in <strong>Subtitling</strong>, <strong>Closed Captioning</strong>, <strong>SDH</strong>, and <strong>QC</strong> across a wide range of movies, TV shows, documentaries, and corporate videos. <br> Also, I am a <strong>Sworn Translator</strong>, I am enrolled in the Sworn Translators Association in Buenos Aires (CTPCBA), so I am equipped to assist you with all your certified translation requirements.",
             /*Education*/
             caeceTitle1: "CAECE University",
             swornTranslator: "Sworn Translator",
             techTranslator: "Technical-Scientific Translator",
-            caeceDates1: "English - Spanish, 2008 - 2013",
+            caeceDates1: "English - Spanish, 2008 - 2012",
             bsasTitle: "Sworn Translators Association in Buenos Aires",
             swornTranslatorCourse: "Member of the Sworn Translators Association in Buenos Aires (CTPCBA)",
             /*Courses*/
@@ -127,8 +128,8 @@ document.addEventListener('DOMContentLoaded', function () {
             by6: "Special guest speakers: Xosé Castro Roig, Liliana Bernardita Mariotto, Damián Santilli, among others.",
             /*Subtitling*/
             contentsub: "Transform your content with precision and professionalism!",
-            contentsub2: "Welcome to my translation and subtitling services! Here, I will transform your audiovisual content into an accessible experience for global audiences:",
-            contentsub3: "I can take your audiovisual project to the next level. With experience in multiple productions, I guarantee high quality results with a personalized and efficient approach.", 
+            contentsub2: "Welcome to my translation and subtitling services! Here, I will transform your audiovisual content into an accessible experience for global audiences.",
+            contentsub3: "I can take your audiovisual project to the next level. With experience in multiple productions, <br> I guarantee high quality results with a personalized and efficient approach.", 
             titlesub2: "I offer the following audiovisual services:",
             subtipelis: "Subtitling of Movies",
             subtitv: "Subtitling of TV <br> and Animation shows" ,
@@ -140,7 +141,7 @@ document.addEventListener('DOMContentLoaded', function () {
             subtionline: "Subtitling of Tutorials <br> and Online Courses",
             subtiytb: "Subtitling of YouTube content",
             /*Coasts*/
-            titlevalor: "What are the costs of an audiovisual project?",
+            titlevalor: "What are the costs <br>of an audiovisual project?",
             textolongvalor: "The costs for a subtitling project depend on multiple aspects, such as:",
             valor1: "Duration",
             pvalor1: "The cost is usually calculated per minute of video.",
@@ -158,7 +159,7 @@ document.addEventListener('DOMContentLoaded', function () {
             certi: "Birth, Death or Marriage certificate",
             balance: "Commercial Balance Sheets and Audit Reports",
             contrato: "General contracts",
-            estatuto: "Articles of Incorporation and Bylaws",
+            estatutos: "Articles of Incorporation and Bylaws",
             declajura: "Income Tax Returns",
             diplo: "Diploma",
             docuacad: "School Documents (Elementary and High School Certificates, University Diplomas and School Transcripts)",
@@ -208,3 +209,21 @@ $(document).ready(function () {
         $('.navbar-collapse').collapse('hide');
     });
 });
+
+const links = document.querySelectorAll('a[href^="#"]');
+
+    for (const link of links) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            const targetId = this.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    }
